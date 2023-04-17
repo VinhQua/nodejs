2d101f93567711322a71a172eafdca600ab6fa3b
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-04-17 21:32:19 +0900

    Add post method

--- app.js
+++ app.js
@@ -2,26 +2,64 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const morgan = require('morgan');
-
+const mongoose = require('mongoose');
+const Blog = require('./modules/blog');
 //connect to mongoDB
-const DBURI = "mongodb+srv://<vinqua>:<Quang123>@cluster0.uvd1bzz.mongodb.net/test"
-
+const DBURI = "mongodb+srv://vinhqua:Quang123@cluster0.uvd1bzz.mongodb.net/test"
+mongoose.connect(DBURI,{useNewUrlParser: true, useUnifiedTopology: true})
+.then((result)=> {
+    console.log('connect to mongoDB')
+    app.listen(3000,()=> console.log('server is running on port 3000'))    
+})
+.catch((err)=> console.log(err))
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.listen(3000, () => {
 
-    console.log('Server is running on port 3000');
-})
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
+//mongoose and mongo sandbox routes 
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: ' New Blog 2',
+        snippet: "About my blog 2",
+        body: "more about my blog 2"
+    })
+    blog.save()
+        .then((result)=> {
+            res.send(result)
+        }).catch((err)=> {
+            console.log(err)
+        })
+})
+app.get('/all-blogs', (req, res) => {
+    Blog.find().sort({createdAt: -1})
+        .then((result)=> {
+            res.render('index',{title: 'All Blogs',blogs:result})
+        })
+        .catch((err)=> {console.log(err)})
+})
+app.post('/add-blog', (req, res) => {
+    console.log(req.body)
+    const blog = new Blog(req.body)
+    blog.save()
+        .then((result)=> {
+            console.log(result)
+            res.redirect('/all-blogs')
+        })
+        .catch((err)=> {console.log(err)})
+})
+// app.get('/blog/:id', (req, res) => {
+//     Blog.findById(req.params.id)
+//         .then((result)=> {
+//             res.send(result)
+//         })
+//         .catch((err)=> {console.log(err)})
+// })
+
 app.get('/', (req, res) => {
-    const blogs =[
-        {title: 'Blog 1',snipet:'snipet1'},
-        {title: 'Blog 2',snipet:'snipet2'},
-        {title: 'Blog 3',snipet:'snipet3'},
-    ]
-    res.render('index',{title: 'Home',blogs});
+    res.redirect('/all-blogs')
 })
 app.get('/about', (req, res) => {
     res.render('about',{title: 'About'});
@@ -31,7 +69,6 @@ app.get('/about-me', (req, res) => {
 })
 app.get('/blog/create', (req, res) => {
     res.render('create',{title: 'Create A Blog'});
-    console.log('new blog')
 })
 app.use((req,res)=>{
     res.render('404',{title: 'Page Not Found'});
